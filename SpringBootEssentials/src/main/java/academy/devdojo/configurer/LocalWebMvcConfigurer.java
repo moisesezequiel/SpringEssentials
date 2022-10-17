7bcd904943157f8ec510219e32441c2bc24f0105
Moises ezequiel <contato.moisesezequiel@outlook.com>
2022-10-17 00:54:25 -0300

    Configuracao customizada para definir Pagina e tamanho a ser carregada default

---
+++ SpringBootEssentials/src/main/java/academy/devdojo/configurer/LocalWebMvcConfigurer.java
@@ -0,0 +1,22 @@
+package academy.devdojo.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class LocalWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(0,5)); //define qual pagina ira carregar por padrao e o seu tamanho
+        resolvers.add(pageHandler);
+
+    }
+}
